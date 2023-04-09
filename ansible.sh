2a1bbf2f87d7c3002b18c2990273d6c01fc99caa
Makson8286 <maksumcherneha70@gmail.com>
2023-04-09 17:56:26 +0000

    ans

---
+++ ansible.sh
@@ -0,0 +1,6 @@
+echo "Hi, its installer Ansible"
+sudo python3 -m pip install --user ansible 
+
+sudo python3 -m pip install --user ansible-core==2.12.3 
+
+sudo python3 -m pip install --upgrade --user ansible
