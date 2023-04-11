e5aa93e4ebdb1d58f756a25c7c888e52e1f2efbc
Makson8286 <129772497+Makson8286@users.noreply.github.com>
2023-04-11 16:27:29 +0300

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,6 +1,5 @@
 echo "Hi, its installer Ansible"
-sudo python3 -m pip install --user ansible 
 
-sudo python3 -m pip install --user ansible-core==2.12.3 
-
-sudo python3 -m pip install --upgrade --user ansible
+sudo apt install -y python3 python3-pip
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt install -y ansible
