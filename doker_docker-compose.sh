a6e453e7dd82a8db2e1088725651b7f9a0cc7dc9
Makson8286 <129772497+Makson8286@users.noreply.github.com>
2023-07-09 15:46:35 +0200

    Update and rename doker.sh to doker_docker-compose.sh

--- doker_docker-compose.sh
+++ doker_docker-compose.sh
@@ -1,7 +1,8 @@
 sudo apt update
-sudo apt install apt-transport-https
+sudo apt install apt-transport-https -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update
-sudo apt install docker-ce
+sudo apt update -y
+sudo apt install docker-ce -y
 sudo usermod -aG docker $USER
+apt install docker-compose -y
