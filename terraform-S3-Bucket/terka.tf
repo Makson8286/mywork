023a35e44d2fb030cb05a78fabbe61e0d6db0233
Makson8286 <129772497+Makson8286@users.noreply.github.com>
2024-02-12 02:23:47 +0100

    Update terka.tf

--- terraform-S3-Bucket/terka.tf
+++ terraform-S3-Bucket/terka.tf
@@ -1,10 +1,10 @@
 provider "aws" {
- region = "us-east-1"
+ region = "your-region"
  access_key = ""
  secret_key = ""
 }
 
 resource "aws_s3_bucket" "tf_state_bucket" {
-  bucket = "omanige"
-  aws s3 cp /home/dik/iam s3://omanige/
+  bucket = "your-name"
+  aws s3 cp /your/path/iam s3://your-name/
 }
