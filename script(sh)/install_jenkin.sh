a5b8a1e6673b77ca6241e6b0e8ce428057813f4c
Makson8286 <129772497+Makson8286@users.noreply.github.com>
2024-05-02 14:50:58 +0200

    Update install_jenkin.sh

--- script(sh)/install_jenkin.sh
+++ script(sh)/install_jenkin.sh
@@ -4,5 +4,5 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
     https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
     /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
-sudo apt-get install fontconfig openjdk-11-jre
+sudo apt-get install fontconfig openjdk-17-jre
 sudo apt-get install jenkins
